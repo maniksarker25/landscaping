7bf4f9efe23df8ab834f1448b1027640e0b70403
manik <maniksarker652@gmail.com>
2026-07-17 16:07:59 +0600

    add whatsapp

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
+import { WhatsAppButton } from "@/components/layout/whatsapp-button";
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <WhatsAppButton />
       <Footer />
     </>
   );
