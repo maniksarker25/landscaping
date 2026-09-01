d81ba18c22d96a3fe005336e5b974e1203067619
hosainahmed <hosainahmed534745@gmail.com>
2026-09-01 20:32:26 +0600

    feat: add helper utility with production base URL configuration

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://192.168.0.114:5000/api/v1";
-export const baseUrl = "https://57n8wl91-5000.inc1.devtunnels.ms/api/v1";
+export const baseUrl = "https://api.dreamfloor.ae/api/v1";
