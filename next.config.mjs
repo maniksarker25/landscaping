bcaddb0326dd7aec26ae5d705984c4187ef69388
hosainahmed <hosainahmed534745@gmail.com>
2026-09-25 17:59:35 +0600

    refactor: remove /pools and /landscaping redirects

--- next.config.mjs
+++ next.config.mjs
@@ -8,20 +8,6 @@ const nextConfig = {
     unoptimized:true
   },
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/pools",
-        destination: "/services/swimming-pool-construction",
-        permanent: true,
-      },
-      {
-        source: "/landscaping",
-        destination: "/services/villa-landscaping",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
